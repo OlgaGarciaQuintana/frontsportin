e9330a2f04d7a1ff0e1e90ed6f2373aea7605873
Olga Garcia <olgarciaq11@gmail.com>
2026-01-29 12:09:33 +0100

    todo arreglado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { Home } from './component/shared/home/home';
 import { ArticuloPlistAdminRouted } from './component/articulo/plist-admin-routed/articulo-plist';
+import { LigaPlistAdminRouted } from './component/liga/plist-admin-routed/liga-plist';
 
 export const routes: Routes = [
     { path: '', component: Home },
-    { path: 'articulo', component: ArticuloPlistAdminRouted},
-    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted}    
+    { path: 'liga', component: LigaPlistAdminRouted },
+    { path: 'liga/:equipo', component: LigaPlistAdminRouted },
+    { path: 'articulo', component: ArticuloPlistAdminRouted },
+    { path: 'articulo/:tipoarticulo', component: ArticuloPlistAdminRouted }
 ];
